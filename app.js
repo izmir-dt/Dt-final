90da5799aaae445f54fe10db695a0061e353348d
İDT <alper3484@gmail.com>
2026-02-08 08:52:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const CONFIG = {
   SPREADSHEET_ID: "1sIzswZnMkyRPJejAsE_ylSKzAF0RmFiACP4jYtz-AE0",
-  // SİZİN VERDİĞİNİZ TEK VE ÇALIŞAN URL
+  // Sizin verdiğiniz çalışan linki buraya kopyalayın
   API_BASE: "https://script.google.com/macros/s/AKfycbxkmxnDtSlfXa008qh_cS2dneTVweaQtMVTIUmOWR1PkAWlHX2EQkd86HwN5X9vZrCp/exec",
   NOTIF_API_BASE: "https://script.google.com/macros/s/AKfycbxkmxnDtSlfXa008qh_cS2dneTVweaQtMVTIUmOWR1PkAWlHX2EQkd86HwN5X9vZrCp/exec",
   
   SHEET_MAIN: "BÜTÜN OYUNLAR",
   SHEET_FIGURAN: "FİGÜRAN LİSTESİ",
   SHEET_NOTIFS: "BİLDİRİMLER",
-  GID: "1233566992",
-  LOG_GID: ""
+  GID: "1233566992"
 };
