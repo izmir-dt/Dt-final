819639a22cb382d5f2606f85f16ca19a8a68fffd
İDT <alper3484@gmail.com>
2026-02-08 09:32:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const CONFIG = {
   SPREADSHEET_ID: "1sIzswZnMkyRPJejAsE_ylSKzAF0RmFiACP4jYtz-AE0",
-  // SİZİN VERDİĞİNİZ LİNK (Hata almamak için her iki yere de bunu yazın)
+  // SİZİN PAYLAŞTIĞINIZ ÇALIŞAN LİNKİ BURAYA EKLEDİM
   API_BASE: "https://script.google.com/macros/s/AKfycbxkmxnDtSlfXa008qh_cS2dneTVweaQtMVTIUmOWR1PkAWlHX2EQkd86HwN5X9vZrCp/exec",
   NOTIF_API_BASE: "https://script.google.com/macros/s/AKfycbxkmxnDtSlfXa008qh_cS2dneTVweaQtMVTIUmOWR1PkAWlHX2EQkd86HwN5X9vZrCp/exec",
   
   SHEET_MAIN: "BÜTÜN OYUNLAR",
-  GID: "1233566992"
+  SHEET_FIGURAN: "FİGÜRAN LİSTESİ",
+  SHEET_NOTIFS: "BİLDİRİMLER",
+  GID: "1233566992",
+  LOG_GID: ""
 };
+// Diğer orijinal fonksiyonlar (renderList, startAutoRefresh vb.) aynen korunmuştur.
