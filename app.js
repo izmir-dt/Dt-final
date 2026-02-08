b8214f939eb400b2c4debfe5489abbb7ab7db152
İDT <alper3484@gmail.com>
2026-02-08 08:23:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   SPREADSHEET_ID: "1sIzswZnMkyRPJejAsE_ylSKzAF0RmFiACP4jYtz-AE0",
-  // SİZİN VERDİĞİNİZ GÜNCEL URL
+  // Sizin güncel exec URL'niz
   API_BASE: "https://script.google.com/macros/s/AKfycbxkmxnDtSlfXa008qh_cS2dneTVweaQtMVTIUmOWR1PkAWlHX2EQkd86HwN5X9vZrCp/exec",
   NOTIF_API_BASE: "https://script.google.com/macros/s/AKfycbxkmxnDtSlfXa008qh_cS2dneTVweaQtMVTIUmOWR1PkAWlHX2EQkd86HwN5X9vZrCp/exec",
   
