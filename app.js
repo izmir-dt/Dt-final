e5cd8a4af247ee5f717f74897aa37caf43e8cdb9
İDT <alper3484@gmail.com>
2026-02-08 07:47:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const CONFIG = {
-  // Buradaki URL'yi kendi 'exec' linkinizle değiştirin
-  API_BASE: "https://script.google.com/macros/s/BURAYA_KENDI_LINKINIZI_YAPISTIRIN/exec",
-  NOTIF_API_BASE: "https://script.google.com/macros/s/BURAYA_KENDI_LINKINIZI_YAPISTIRIN/exec",
-  // ... diğer ayarlar
+  SPREADSHEET_ID: "1sIzswZnMkyRPJejAsE_ylSKzAF0RmFiACP4jYtz-AE0",
+  // SİZİN VERDİĞİNİZ GÜNCEL URL:
+  API_BASE: "https://script.google.com/macros/s/AKfycbxkmxnDtSlfXa008qh_cS2dneTVweaQtMVTIUmOWR1PkAWlHX2EQkd86HwN5X9vZrCp/exec",
+  NOTIF_API_BASE: "https://script.google.com/macros/s/AKfycbxkmxnDtSlfXa008qh_cS2dneTVweaQtMVTIUmOWR1PkAWlHX2EQkd86HwN5X9vZrCp/exec",
+  
+  SHEET_MAIN: "BÜTÜN OYUNLAR",
+  SHEET_FIGURAN: "FİGÜRAN LİSTESİ",
+  SHEET_NOTIFS: "BİLDİRİMLER",
+  GID: "1233566992"
 };
